90407689f942e3fc51582bf7322ac3b32174c114
leonawicz <matt.rstat@gmail.com>
2017-10-25 12:54:55 -0600

    Add package documentation.

---
+++ R/snaputils.R
@@ -0,0 +1,13 @@
+globalVariables(c(".", ".data"))
+
+#' SNAP-specific utilities for Shiny apps.
+#'
+#' The \code{snaputils} package common utility functions, settings and references used across multiple Shiny apps
+#' specific to a SNAP context. This package inherents from the apputils package and is a satellite member of the SNAPverse collection of R packages.
+#'
+#' @docType package
+#' @name snaputils
+NULL
+
+#' @importFrom magrittr %>%
+NULL
