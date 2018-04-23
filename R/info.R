a471f06aac3f2e4947cc744bf81680c856b73eec
leonawicz <matt.rstat@gmail.com>
2018-04-23 09:49:35 -0600

    Update url.

--- R/info.R
+++ R/info.R
@@ -171,7 +171,7 @@ snapp_showcase <- function(drop = NULL){
     img_url = c(
       "https://github.com/ua-snap/shiny-apps/raw/master/_images/small/rvdist.jpg",
       "https://raw.githubusercontent.com/leonawicz/dash/master/images/_climdist_small.png",
-      "https://raw.githubusercontent.com/leonawicz/jfsp/master/_jfsp_small.png",
+      "https://raw.githubusercontent.com/leonawicz/jfsp-archive/master/_jfsp_small.png",
       "https://raw.githubusercontent.com/leonawicz/ar5eval/master/_ar5eval_small.png",
       "https://github.com/ua-snap/shiny-apps/raw/master/_images/small/cc4liteFinal.jpg",
       "https://github.com/ua-snap/shiny-apps/raw/master/_images/small/ex_leaflet.jpg",
